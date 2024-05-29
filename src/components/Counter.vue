4a4d63c4227624952198963baf9efb18d101074d
kanghanstar <kanghanstar@gmail.com>
2024-05-29 15:29:11 +0900

    [코드정리] prettier 적용

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,18 +1,18 @@
 <template>
-  <h1 @click="increase">{{ count }}</h1>
+    <h1 @click="increase">{{ count }}</h1>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
-  methods: {
-    increase() {
-      this.count += 1
-    }
-  }
-}
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    methods: {
+        increase() {
+            this.count += 1;
+        },
+    },
+};
 </script>
